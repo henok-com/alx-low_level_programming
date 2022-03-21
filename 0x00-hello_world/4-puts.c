ded28c657e305d1173f570e9ecc423f2e0507b3d
henaman <henaman4885@gamil.com>
2022-03-21 08:23:23 -0700

    bugs fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*opening main function
- *
+/*
+ * opening main function
  */
 int main() {
 	/*
 	 * printing a sentence in the screen using puts
 	 */
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
