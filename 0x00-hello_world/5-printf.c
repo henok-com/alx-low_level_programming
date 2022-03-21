f1ff6ff95ec224dc199bd163590e75178227e922
henaman <henaman4885@gamil.com>
2022-03-21 12:03:48 -0700

    using printf is simple and fun in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main funcrion entry
+ * printing text to the screen using printf function not puts
+ * returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
