d3a4740cf6fa0d7dad66427908185c7ce69bcf73
henaman <henaman4885@gamil.com>
2022-03-21 08:13:03 -0700

    using puts rather than printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//opening main function
+int main() {
+	//printing a sentence in the screen using puts
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
